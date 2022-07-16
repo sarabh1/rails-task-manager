7cddf2d2b83b1003134db47ee4b0dfab00729e1a
sara bahhar <bahhar.sara95@gmail.com>
2022-07-16 11:13:42 +0200

    Adding list  show create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
+
+
+    # get "tasks" => "tasks#index" // tous les restaus
+    # get "tasks/:id" => "tasks#show" // un restau en particulier
+    # get "tasks/new" => "tasks#new" // creer un restaurant etape 1
+    # post "tasks" => "tasks#create" // creer un restaurant etape 2
+    # get "tasks/:id/edit" => "tasks#edit" // updater un restaurant etape 1 // edit pour ne pas avoir de conflit avec le show
+    # patch "tasks" => "tasks#update" // updater un restaurant etape 2
+    # delete "tasks/:id" => "tasks#delete" // supprimer une task
